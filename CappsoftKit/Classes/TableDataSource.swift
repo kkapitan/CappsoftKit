7e2c6d133ac307fb063770c7f5883cea6e9c8ddf
Krzysztof <krzysztof.kapitan@gmail.com>
2016-12-28 21:11:15 +0100

    Add new file for descriptor class

--- CappsoftKit/Classes/TableDataSource.swift
+++ CappsoftKit/Classes/TableDataSource.swift
@@ -8,29 +8,6 @@
 
 import UIKit
 
-public class CellDescriptor<T> {
-    let cellClass: UITableViewCell.Type
-    let reuseIdentifier: String
-    
-    let configureCell: (UITableViewCell, IndexPath, T) -> ()
-    
-    public init<Base: UITableViewCell>(cellClass: Base.Type, configure: @escaping (Base, IndexPath, T) -> ()) where Base: CellType, Base.ViewModel.Model == T {
-        self.cellClass = cellClass
-        self.reuseIdentifier = cellClass.reuseIdentifier
-        self.configureCell = { cell, indexPath, model in
-            configure(cell as! Base, indexPath, model)
-        }
-    }
-    
-    public convenience init<Base: UITableViewCell>(cellClass: Base.Type) where Base: CellType, Base.ViewModel.Model == T {
-        let configure: (Base, IndexPath, T) -> () = { cell, _, model in
-            let viewModel = Base.ViewModel(model: model)
-            cell.populateWithViewModel(viewModel: viewModel)
-        }
-        self.init(cellClass: cellClass, configure: configure)
-    }
-}
-
 public class TableDataSource<Provider: DataProvidable> : DataSource<Provider>, UITableViewDataSource {
     public typealias Descriptor = CellDescriptor<Provider.Element>
     public typealias DescriptorSelector = (Provider.Element, IndexPath) -> Descriptor
